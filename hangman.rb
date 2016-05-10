ee2f3a967fc2a5d625987183e9c9a74821a2a42e
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-05-10 20:35:57 +0200

    Added save/load feature

--- hangman.rb
+++ hangman.rb
@@ -4,8 +4,30 @@ class Hangman
   def initialize
     @dictionary_path = "5desk.txt"
     @limit = 10
-    @save_direcory = "save_state.yaml"
+    @save_directory = "save_state.yaml"
+    @password = choose_password.split(//)  # array of correct letters
+    @guessing_board = @password.map { "_" } # current board and correct guesses    
+    @incorrect_guesses = []  # prevent duplicates
     main_menu
+  end
+  
+  protected
+  
+  def new_game
+    while @limit > 0
+      status = continue
+      if status
+        puts "Congratulations, you win!"
+        draw_board
+        break
+      else
+        puts "Incorrect guesses left: #{@limit}"
+      end
+    end
+    if @limit == 0
+      puts "You lose!"
+      puts "The correct word was: #{@password.join}"
+    end
   end
   
   private
@@ -18,7 +40,7 @@ class Hangman
     puts "2 Load Game"
     choice = get_mode
     if choice == "1"
-      new_game
+      self.new_game
     else
       load_game
     end
@@ -33,84 +55,74 @@ class Hangman
     return choice
   end
   
-  def new_game
-    dictionary = File.open(@dictionary_path, 'r')
-    password = choose_password(dictionary).split(//)  # array of correct
-    guessing_board = password.map { "_" } # current board and correct guesses
-    incorrect_guesses = []  # to prevent duplicates
-    while @limit > 0
-      status = continue(password, guessing_board, incorrect_guesses)
-      if status
-        puts "Congratulations, you win!"
-        break
-      else
-        puts "Incorrect guesses left: #{@limit}"
-      end
-    end
-    if @limit == 0
-      puts "You lose!"
-      puts "The correct word was: #{password.join}"
-    end
-  end
-  
-  def continue (password, guessing_board, incorrect_guesses)
-    guess = get_guess
-    while duplicate?(guess, guessing_board, incorrect_guesses)
-      puts "You've already picked this letter. Pick another one:"
-      guess = get_guess
-    end
-    check_guess(password, guess, guessing_board, incorrect_guesses)
-    draw_hr(password.length)
-    puts guessing_board.join(" ")
-    draw_hr(password.length)
-    if password.join == guessing_board.join
-      return true
-    else
-      puts "Incorrectly guessed letters: #{incorrect_guesses.join(", ")}"
-    end
+  def continue
+    draw_board
+    puts "Incorrectly guessed letters: #{@incorrect_guesses.join(", ")}"
+    guess = get_input
+    guess = get_input while duplicate?(guess)
+    check_guess(guess)
+    return true if @password.join == @guessing_board.join
   end
   
   def load_game
     puts "Loading..."
+    saved_state = File.open("save_state.yaml", "r")
+    game = saved_state.read
+    YAML::load(game).new_game
   end
   
-  def choose_password (dict)
+  def choose_password
     puts "Loading dictionary..."
-    valid_passwords = dict.select do |x|
+    dictionary = File.open(@dictionary_path, 'r')
+    valid_passwords = dictionary.select do |x|
       x.length > 5 && x.length < 12 && x[0] !~ /[A-Z]/
     end
     return valid_passwords.sample.chomp
   end
   
-  def get_guess
-    puts "\nGuess the letter:"
+  def get_input
+    puts "\nGuess the letter to continue or enter 'save' to save & exit:"
     guess = gets.chomp.downcase
+    save_game if guess == 'save'
     until guess =~ /^[a-z]$/
-      puts "Invalid input. Enter a single letter."
+      puts "Enter a single letter."
       guess = gets.chomp.downcase
     end
     return guess
   end
   
-  def check_guess (password, guess, correct, incorrect)
-    if password.include?(guess)
-      puts "Guess: #{guess}"
-      password.each_with_index do |elem, index|
-        correct[index] = guess if elem == guess
+  def check_guess (guess)
+    if @password.include?(guess)
+      @password.each_with_index do |elem, index|
+        @guessing_board[index] = guess if elem == guess
       end
     else
-      incorrect.push(guess)
+      @incorrect_guesses.push(guess)
       @limit -= 1
     end
+  end
+  
+  def draw_board
+    draw_hr(@password.length)
+    puts @guessing_board.join(" ")
+    draw_hr(@password.length)
   end
   
   def draw_hr (length)
     puts "".center(length * 2, "=")
   end
   
-  def duplicate? (guess, correct, incorrect)
+  def duplicate? (guess)
     result = false
-    return true if correct.include?(guess) || incorrect.include?(guess)
+    return true if @guessing_board.include?(guess) || @incorrect_guesses.include?(guess)
+  end
+  
+  def save_game
+    File.open(@save_directory, "w") do |file|
+      save_state = YAML::dump(self)
+      file.write(save_state)
+      abort "Game saved. See you soon..."
+    end
   end
 end
 game = Hangman.new
